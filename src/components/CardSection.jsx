1e782e60a790db95fc406cf01451ec38337e26f0
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-24 00:59:23 +0600

    card redesing

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Drawer from './Drawer';
+import { BsTelegram } from 'react-icons/bs';
 
 const CardSection = () => {
     return (
@@ -14,47 +15,83 @@ const CardSection = () => {
                     <div className='flex'>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
                     </div>
@@ -62,49 +99,86 @@ const CardSection = () => {
                     <div className='flex'>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
 
                         <div className='ml-3 mt-2'>
-                            <div className="card w-full bg-base-100 shadow-xl border">
+                            <div className="card w-full bg-base-100 shadow-xl border relative">
                                 <figure className="p-3">
                                     <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
                                 </figure>
                                 <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Shoes!</h2>
-                                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                                    <div className="card-actions">
+                                    <p className='font-bold'>
+                                        Favipiravir 400mg (Fabiflu) Tablets
+                                    </p>
+                                    <h2 className="card-title text-red-600 font-bold">Rs 1,775 / <span className='text-base text-slate-500'>stripe</span></h2>
+                                    <p className='font-bold'>Glenmark Pharmaceutical Limited</p>
+                                    <p className='uppercase text-sm text-slate-500 mb-5'>parvat patiya, surat, gujrat</p>
+                                    {/* <div className="card-actions">
                                         <button className="btn btn-primary">Buy Now</button>
+                                    </div> */}
+                                    <div className='flex justify-center items-center space-x-3 mb-[-10px]'>
+                                        <BsTelegram className='text-sky-500' size={40} />
+                                        <p className='text-sky-500 text-xl'>Contact Supplier</p>
                                     </div>
+
                                 </div>
+
+                                <hr className='bg-sky-500 h-1 border w-full absolute bottom-24' />
+                                <hr className='bg-[#750B0B] h-3 border w-full' />
                             </div>
                         </div>
+                        
                     </div>
                 </div>
 
