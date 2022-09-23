743ad7def57b13465285a3613155a64e7d41c2c1
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-23 23:15:24 +0600

    navbar edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,24 @@ import React from 'react';
 const Navbar = () => {
     return (
         <section>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-[#750B0B] border-2">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Homepage</a></li>
-                            <li><a>Portfolio</a></li>
-                            <li><a>About</a></li>
-                        </ul>
                     </div>
-                    <div>
+                    <div className='font-medium text-2xl'>
                         Medical Darpan
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a className="btn btn-ghost normal-case text-xl">Home</a>
+                    <a className="btn btn-ghost normal-case text-xl">Product</a>
+                    <a className="btn btn-ghost normal-case text-xl">Distributor</a>
+                    <a className="btn btn-ghost normal-case text-xl">Manufacturers</a>
+                    <a className="btn btn-ghost normal-case text-xl">About Us</a>
+                    <a className="btn btn-ghost normal-case text-xl">Blog</a>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
